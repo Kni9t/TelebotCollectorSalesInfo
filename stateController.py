fe75e096fc1d13758a5f49690513657d40d68b79
Kni9t <clone111@bk.ru>
2025-07-31 14:11:29 +0300

    add stateController

---
+++ stateController.py
@@ -0,0 +1,66 @@
+import os
+import json
+from datetime import datetime
+
+import jsonController as JC
+
+'''
+'1234536': {
+        'authorizationState': False,
+        'salesCollectState': False,
+        'selectedMarket': 'market name' or None
+    }
+'''
+
+class stateController(JC.jsonController):
+    def setUserStats(self, idUser, stats, value):
+        dateFromFiles = self.getDate()
+        change = False
+
+        for id in dateFromFiles:
+            if id == idUser:
+                dateFromFiles[id][stats] = value
+                change = True
+
+        if (change == False):
+            dateFromFiles[idUser] = {
+                'authorizationState': False,
+                'salesCollectState': False,
+                'selectedMarket': None
+            }
+            dateFromFiles[idUser][stats] = value
+        
+        self.writeDate(dateFromFiles)
+
+    def getAuthorizationUserState(self, idUser):
+        dateFromFiles = self.getDate()
+
+        for id in dateFromFiles:
+            if (id == idUser):
+                return dateFromFiles[id]['authorizationState']
+            
+    def getAuthorizatMarket(self, idUser):
+        dateFromFiles = self.getDate()
+
+        for id in dateFromFiles:
+            if (id == idUser):
+                return dateFromFiles[id]['selectedMarket']
+    
+    def getSalesCollectState(self, idUser):
+        dateFromFiles = self.getDate()
+
+        for id in dateFromFiles:
+            if (id == idUser):
+                return dateFromFiles[id]['salesCollectState']
+        
+    def addDate(self, date):
+        if (os.path.isfile(self.fileName)):
+            dateFromFiles = self.getDate()
+            for key in date:
+                dateFromFiles[str(key)] = date[str(key)]
+
+            with open(self.fileName, "w", encoding='utf8') as outfile:
+                outfile.write(json.dumps(dateFromFiles))
+                outfile.close()
+        else:
+            self.writeDate(date)
